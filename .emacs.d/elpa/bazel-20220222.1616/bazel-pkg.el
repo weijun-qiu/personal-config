1ee432a67242fb5af75ceeef925b93c8c66118d6
Weijun Qiu <wellkin.ball@outlook.com>
2022-04-20 00:05:25 +0800

    Add .gitignore and cleanup redundant emacs files

---
+++ .emacs.d/elpa/bazel-20220222.1616/bazel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bazel" "20220222.1616" "Bazel support for Emacs" '((emacs "26.1")) :commit "e07a16666154c8ddc65ddaae599d58b25727350d" :keywords '("build tools" "languages") :url "https://github.com/bazelbuild/emacs-bazel-mode")
