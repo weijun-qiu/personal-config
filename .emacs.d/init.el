0cfce5165532f2b8f3528a482cd92a6a53ff2bc2
Weijun Qiu <wellkin.ball@outlook.com>
2022-09-27 23:51:19 +0800

    Add lsp-ivy and helm-company to package-selected-packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,7 +15,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit bazel helm gnu-elpa-keyring-update lsp-ui helm-lsp lsp-mode yaml-mode dockerfile-mode ivy irony-eldoc flycheck-irony company-irony company google-c-style exec-path-from-shell clang-format irony cmake-mode zenburn-theme)))
+   '(magit bazel lsp-ivy helm-company helm gnu-elpa-keyring-update lsp-ui helm-lsp lsp-mode yaml-mode dockerfile-mode ivy irony-eldoc flycheck-irony company-irony company google-c-style exec-path-from-shell clang-format irony cmake-mode zenburn-theme)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
