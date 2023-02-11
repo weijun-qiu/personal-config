e70fb0290fe6d9506cf7e17d51d0c9748fda0697
Weijun Qiu <wellkin.ball@outlook.com>
2023-02-11 12:19:15 +0800

    Add script to setup powershell profiles

---
+++ powershell/setup_profile.ps1
@@ -0,0 +1,5 @@
+﻿if (Test-Path -Path $PROFILE) {
+  Move-Item -Path $PROFILE -Destination $PROFILE".bak" -Force
+} 
+$script_path = $(Split-Path $MyInvocation.MyCommand.Path -Parent)
+Copy-Item -Path "$($script_path)\profile.ps1" -Destination $PROFILE
