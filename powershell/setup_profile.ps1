28850189a77072b772bb096077067c1b2f2087f7
Weijun Qiu <wellkin.ball@outlook.com>
2023-02-14 19:58:00 +0800

    fix: fix setup_profile.ps1

--- powershell/setup_profile.ps1
+++ powershell/setup_profile.ps1
@@ -2,4 +2,8 @@
   Move-Item -Path $PROFILE -Destination $PROFILE".bak" -Force
 } 
 $script_path = $(Split-Path $MyInvocation.MyCommand.Path -Parent)
+$dest_dir= $(Split-Path $PROFILE -Parent)
+if (!(Test-Path -Path $PROFILE)) {
+    New-Item -Path $dest_dir -ItemType Directory -Force
+}
 Copy-Item -Path "$($script_path)\profile.ps1" -Destination $PROFILE
