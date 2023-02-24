d3ad8650583bdea507255cdad114980f42057018
Weijun Qiu <wellkin.ball@outlook.com>
2023-02-24 14:29:28 +0800

    fix: fix windows sizing logic on Windows

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,12 +19,25 @@
 (setq-default package-selected-packages '(magit bazel helm gnu-elpa-keyring-update lsp-ui helm-lsp lsp-mode yaml-mode dockerfile-mode ivy irony-eldoc flycheck-irony company-irony company google-c-style exec-path-from-shell clang-format irony cmake-mode zenburn-theme))
 
 ;; Set startup window size
-(when (or window-system (daemonp))
-  (setq default-frame-alist '(
-                              (width . 150)
-                              (height . 800)
-			      ))
-  )
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 120))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
 
 ;; Remove undeeded ui components
 (setq inhibit-startup-screen t)
