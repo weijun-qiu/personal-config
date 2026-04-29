551b205b1a4afc7cc42975223a20e9eb9e935812
Weijun Qiu <wellkin.ball@outlook.com>
2026-04-29 15:04:42 +0800

    Remove emacs elpa

--- .emacs.d/elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "d0ddef58128799dcb2a139365c1eecbb6c046aab")
